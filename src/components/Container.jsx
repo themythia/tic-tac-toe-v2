67aa9fbd5f57e209605805bcc5d2a152224e6dbb
Emir Al <mythia@windowslive.com>
2022-07-21 12:06:35 +0300

    docs: update readme

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -4,8 +4,6 @@ import { GameContext } from '../contexts/GameContext';
 const Container = ({ children }) => {
   const { gameState, dispatch } = useContext(GameContext);
 
-  console.log('gameState:', gameState);
-
   useEffect(() => {
     if (gameState.turn === 'cpu') {
       dispatch({ type: 'CPU_NEXT_MOVE' });
